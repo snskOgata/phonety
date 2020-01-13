d9265a56997e58018d878f50f9095facbf417308
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-13 14:57:13 +0900

    Add redirect to users#show after sign_up/sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # 新規登録後、ユーザーページへ
+  def after_sign_up_path_for(resource)
+    "/users/#{current_user.id}"
+  end
+  # ログイン後、ユーザーページへ
+  def after_sign_in_path_for(resource)
+      "/users/#{current_user.id}"
+  end
+
   protected
     def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
