461eb7983096f5129fc833d903f22985d30c7a17
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-13 20:24:34 +0900

    Add ajax for update_goal

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -2,15 +2,26 @@ $(function () {
 
   $('#goal-update-btn').hide();
 
+  // 編集ボタンが押された場合
   $('#goal-edit-btn').on('click', function () {
     $('#goal-update-btn').show();
     $('#goal-edit-btn').hide();
     $('#goal-text').prop("disabled", false).css('background-color', 'white').focus();
   });
 
+  // 更新ボタンが押された場合
   $('#goal-update-btn').on('click', function () {
-    $('#goal-edit-btn').show();
-    $('#goal-update-btn').hide();
-    $('#goal-text').prop("disabled", true).css('background-color', '#f5f5f5');
+    $.ajax({
+      url: location.pathname + '/update_goal',
+      type: 'PATCH',
+      data: { goal: $('#goal-text').val() },
+    }).done(function () {
+      $('#goal-edit-btn').show();
+      $('#goal-update-btn').hide();
+      $('#goal-text').prop("disabled", true).css('background-color', '#f5f5f5');
+    })
+      .fail(function () {
+        alert('更新に失敗しました。')
+      })
   });
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
-  resources :users, only: :show
+  resources :users, only: :show do
+    patch 'update_goal', to: 'users#update_goal'
+  end
   namespace :api do
     resources :compares, only: :create
   end
