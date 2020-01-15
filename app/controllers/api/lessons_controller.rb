b7efeb06db411bded43459fd58ba805341fa2cd9
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-15 16:00:19 +0900

    Create reviews lessons#create

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -7,6 +7,11 @@ class Api::LessonsController < ApplicationController
       else
         # 新規ならば登録
         if lesson = Lesson.create(lesson_params)
+          today = Date.today
+          dates_since = [1, 3, 7, 14, 21, 28]
+          (0..5).each do |i|
+            Review.create(user_id: current_user.id, lesson_id: lesson.id, count: (i + 1), date: (today + dates_since[i]))
+          end
         else
           flash.now("保存に失敗しました")
         end
