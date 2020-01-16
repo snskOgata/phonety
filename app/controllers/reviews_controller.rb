d103ecc48c40477a38f56751e73ce11f8cf0401e
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-16 14:46:04 +0900

    Add show today's reviews to do

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
   def today
+    @reviews = current_user.reviews.where("(done = ?) AND (date <= ?)", false, Date.today).includes(:lesson)
   end
 end
