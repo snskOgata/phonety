0d304f5911792777e6c191ce6a728ecfa6600ea0
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-17 15:42:55 +0900

    Add Association to user and study_record

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -1,2 +1,3 @@
 class StudyRecord < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 20}
   has_many :lessons, dependent: :destroy
   has_many :reviews
+  has_one :study_record
 end
