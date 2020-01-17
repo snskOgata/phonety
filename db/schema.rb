cacdafea31418689b808b4b7dc14f0c70fc30ad6
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-17 15:37:52 +0900

    Migrate study_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_061103) do
+ActiveRecord::Schema.define(version: 2020_01_17_063529) do
 
   create_table "lessons", force: :cascade do |t|
     t.integer "user_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_061103) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "study_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
