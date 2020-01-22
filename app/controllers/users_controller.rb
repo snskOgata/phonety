18a14e53ddedfa4b5d3d61ebb9e26ca049cd701e
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-22 12:26:29 +0900

    Add badge for today_lessons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @today_reviewing = current_user.reviews.where("(done = ?) AND (date <= ?)", false, Date.today)
     @today_reviewed = current_user.reviews.where("(done = ?) AND (done_date = ?)", true, Date.today)
+    @today_lessons = current_user.lessons.where(created_at: Time.zone.now.all_day)
   end
 
   def update_goal
