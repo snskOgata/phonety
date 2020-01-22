e46ac4a14d363b0fcd09c507494675ee20d635d7
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-22 12:13:17 +0900

    Add today's reviewing badge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def show
+    @reviews = current_user.reviews.where("(done = ?) AND (date <= ?)", false, Date.today)
+  end
+
   def update_goal
     unless current_user.update(goal: params[:goal])
       flash.now[:alert] = "更新に失敗しました"
